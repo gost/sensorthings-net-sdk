91848284f013ef32b6d738f6206918f047e82164
bert <btemme@hotmail.com>
2017-10-03 13:42:45 +0200

    clean up

--- src/sensorthings-net-sdk/Properties/AssemblyInfo.cs
+++ src/sensorthings-net-sdk/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("sensorthings-net-sdk")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OGC SensorThings client for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Geodan")]
 [assembly: AssemblyProduct("sensorthings-net-sdk")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.20.0.0")]
