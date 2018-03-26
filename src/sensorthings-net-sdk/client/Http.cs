5db0c1c5b6830b1302ea10a4a2f452727dffc3eb
Tim Ebben <ebben.tim@gmail.com>
2018-03-26 16:19:15 +0200

    reuse HttpClient

--- src/sensorthings-net-sdk/client/Http.cs
+++ src/sensorthings-net-sdk/client/Http.cs
@@ -7,10 +7,11 @@ namespace SensorThings.Client
 {
     public static class Http
     {
+        private static HttpClient Client = new HttpClient();
+
         public static async Task<T> GetJson<T>(string url)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(url);
+            var response = await Client.GetAsync(url);
             var strJson = await response.Content.ReadAsStringAsync();
             var items = JsonConvert.DeserializeObject<T>(strJson);
             return items;
@@ -18,7 +19,6 @@ namespace SensorThings.Client
 
         public static async Task<T> PostJson<T>(string url, T entity)
         {
-            var client = new HttpClient();
             var serialized = JsonConvert.SerializeObject(entity, Formatting.None,
                             new JsonSerializerSettings
                             {
@@ -28,7 +28,7 @@ namespace SensorThings.Client
             var buffer = System.Text.Encoding.UTF8.GetBytes(serialized);
             var byteContent = new ByteArrayContent(buffer);
             byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            var responseMessage = await client.PostAsync(url, byteContent);
+            var responseMessage = await Client.PostAsync(url, byteContent);
             var response = await responseMessage.Content.ReadAsStringAsync();
             var item = JsonConvert.DeserializeObject<T>(response);
             return item;
