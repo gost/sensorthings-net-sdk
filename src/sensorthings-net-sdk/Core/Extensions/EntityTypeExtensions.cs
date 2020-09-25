d1d60d734d6744568d436149b67691952bc3ec7a
Danny Lade <danny.lade@iteratec.com>
2020-09-25 14:45:01 +0200

    Cleanup code: Replace EntityType by real Type.

--- src/sensorthings-net-sdk/Core/Extensions/EntityTypeExtensions.cs
+++ src/sensorthings-net-sdk/Core/Extensions/EntityTypeExtensions.cs
@@ -1,34 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using SensorThings.Core;
 
 namespace sensorthings.Extensions
 {
     public static class EntityTypeExtensions
     {
-        public static string GetString(this EntityType entityType, bool collection)
-        {
-            switch (entityType)
-            {
-                case EntityType.None:
-                    return "";
-                case EntityType.Thing:
-                    return collection ? "Things" : "Thing";
-                case EntityType.Location:
-                    return collection ? "Locations" : "Location";
-                case EntityType.HistoricalLocation:
-                    return collection ? "HistoricalLocations" : "HistoricalLocation";
-                case EntityType.Datastream:
-                    return collection ? "Datastreams" : "Datastream";
-                case EntityType.Sensor:
-                    return collection ? "Sensors" : "Sensor";
-                case EntityType.ObservedProperty:
-                    return collection ? "ObservedProperties" : "ObservedProperty";
-                case EntityType.Observation:
-                    return collection ? "Observations" : "Observation";
-                case EntityType.FeatureOfInterest:
-                    return collection ? "FeaturesOfInterest" : "FeatureOfInterest";
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(entityType), entityType, null);
+        private static readonly IDictionary<string, bool> Map =
+            new Dictionary<string, bool> {
+                { $"{nameof(Thing)}/{nameof(HistoricalLocation)}", false },
+                { $"{nameof(Thing)}/{nameof(Datastream)}", false },
+                { $"{nameof(Thing)}/{nameof(Location)}", true },
+                { $"{nameof(Location)}/{nameof(HistoricalLocation)}", true },
+                { $"{nameof(Location)}/{nameof(Thing)}", true },
+                { $"{nameof(HistoricalLocation)}/{nameof(Thing)}", true },
+                { $"{nameof(HistoricalLocation)}/{nameof(Location)}", true },
+                { $"{nameof(Datastream)}/{nameof(ObservedProperty)}", true },
+                { $"{nameof(Datastream)}/{nameof(Sensor)}", true },
+                { $"{nameof(Datastream)}/{nameof(Thing)}", true },
+                { $"{nameof(Datastream)}/{nameof(Observation)}", false },
+                { $"{nameof(Sensor)}/{nameof(Datastream)}", false },
+                { $"{nameof(ObservedProperty)}/{nameof(Datastream)}", false },
+                { $"{nameof(Observation)}/{nameof(Datastream)}", true },
+                { $"{nameof(Observation)}/{nameof(FeatureOfInterest)}", true },
+                { $"{nameof(FeatureOfInterest)}/{nameof(Observation)}", false },
+            };
+
+        public static string GetString(this Type type, bool collection) {
+            if (!type.IsSubclassOf(typeof(AbstractEntity))) {
+                return "";
             }
+            return collection ? $"{type.Name}s" : type.Name;
+        }
+
+        public static string GetString(this Type get, Type by) {
+            var key = $"{get.Name}/{by.Name}";
+            if (!Map.ContainsKey(key)) {
+                throw new ArgumentException("Path does not exist");
+            }
+            return get.GetString(Map["key"]);
         }
     }
 }
--- src/sensorthings-net-sdk/client/SensorThingsClient.cs
+++ src/sensorthings-net-sdk/client/SensorThingsClient.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
-using sensorthings;
+
 using sensorthings.Core;
+
+using SensorThings.Core;
+
 using sensorthings.Extensions;
 using sensorthings.ODATA;
-using SensorThings.Core;
 
 namespace SensorThings.Client
 {
@@ -13,224 +15,197 @@ namespace SensorThings.Client
 
         public string Server { get; set; }
 
-        public SensorThingsClient(string server)
-        {
+        public SensorThingsClient(string server) {
             Server = server;
-            Task.Run(async () =>
-            {
-                var response = await Http.GetJson<HomeDocument>(Server);
-                if (!response.Success)
-                {
-                    throw new Exception("Unable to get home document");
-                }
+            Task.Run(async () => {
+                    var response = await Http.GetJson<HomeDocument>(Server);
+                    if (!response.Success) {
+                        throw new Exception("Unable to get home document");
+                    }
 
-                return _homedoc = response.Result;
-            }).Wait();
+                    return _homedoc = response.Result;
+                })
+                .Wait();
         }
 
-        public async Task<Response<Thing>> GetThing(string id, OdataQuery odata = null)
-        {
-            return await Get<Thing>(EntityType.Thing, id, odata);
+        public async Task<Response<Thing>> GetThing(string id, OdataQuery odata = null) {
+            return await Get<Thing>(typeof(Thing), null, id, odata);
         }
 
-        public async Task<Response<Thing>> GetThingByDatastream(string id, OdataQuery odata = null)
-        {
-            return await Get<Thing>(EntityType.Thing, EntityType.Datastream, id, odata);
+        public async Task<Response<Thing>> GetThingByDatastream(string id, OdataQuery odata = null) {
+            return await Get<Thing>(typeof(Thing), typeof(Datastream), id, odata);
         }
 
-        public async Task<Response<Thing>> GetThingByHistoricalLocation(string id, OdataQuery odata = null)
-        {
-            return await Get<Thing>(EntityType.Thing, EntityType.HistoricalLocation, id, odata);
+        public async Task<Response<Thing>> GetThingByHistoricalLocation(string id, OdataQuery odata = null) {
+            return await Get<Thing>(typeof(Thing), typeof(HistoricalLocation), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Thing>>> GetThingCollection(OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Thing>>(EntityType.Thing, odata);
+        public async Task<Response<SensorThingsCollection<Thing>>> GetThingCollection(OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Thing>>(typeof(Thing), null, null, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Thing>>> GetThingCollectionByLocation(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Thing>>(EntityType.Thing, EntityType.Location, id, odata);
+        public async Task<Response<SensorThingsCollection<Thing>>> GetThingCollectionByLocation(
+            string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Thing>>(typeof(Thing), typeof(Location), id, odata);
         }
 
-        public async Task<Response<Location>> GetLocation(string id, OdataQuery odata = null)
-        { 
-            return await Get<Location>(EntityType.Location, id, odata);
+        public async Task<Response<Location>> GetLocation(string id, OdataQuery odata = null) {
+            return await Get<Location>(typeof(Location), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Location>>> GetLocationCollection(OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Location>>(EntityType.Location, odata);
+        public async Task<Response<SensorThingsCollection<Location>>> GetLocationCollection(OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Location>>(typeof(Location), odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Location>>> GetLocationCollectionByHistoricalLocation(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Location>>(EntityType.Location, EntityType.HistoricalLocation, id, odata);
+        public async Task<Response<SensorThingsCollection<Location>>>
+            GetLocationCollectionByHistoricalLocation(string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Location>>(typeof(Location), typeof(HistoricalLocation), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Location>>> GetLocationCollectionByThing(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Location>>(EntityType.Location, EntityType.Thing, id, odata);
+        public async Task<Response<SensorThingsCollection<Location>>> GetLocationCollectionByThing(
+            string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Location>>(typeof(Location), typeof(Thing), id, odata);
         }
 
-        public async Task<Response<HistoricalLocation>> GetHistoricalLocation(string id, OdataQuery odata = null)
-        {
-            return await Get<HistoricalLocation>(EntityType.HistoricalLocation, id, odata);
+        public async Task<Response<HistoricalLocation>> GetHistoricalLocation(string id, OdataQuery odata = null) {
+            return await Get<HistoricalLocation>(typeof(HistoricalLocation), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<HistoricalLocation>>> GetHistoricalLocationsCollection(OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<HistoricalLocation>>(EntityType.HistoricalLocation, odata);            
+        public async Task<Response<SensorThingsCollection<HistoricalLocation>>>
+            GetHistoricalLocationsCollection(OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<HistoricalLocation>>(typeof(HistoricalLocation), odata);
         }
 
-        public async Task<Response<SensorThingsCollection<HistoricalLocation>>> GetHistoricalLocationsCollectionByLocation(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<HistoricalLocation>>(EntityType.HistoricalLocation, EntityType.Location, id, odata);
+        public async Task<Response<SensorThingsCollection<HistoricalLocation>>>
+            GetHistoricalLocationsCollectionByLocation(string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<HistoricalLocation>>(typeof(HistoricalLocation), typeof(Location),
+                id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<HistoricalLocation>>> GetHistoricalLocationsCollectionByThing(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<HistoricalLocation>>(EntityType.HistoricalLocation, EntityType.Thing, id, odata);
+        public async Task<Response<SensorThingsCollection<HistoricalLocation>>>
+            GetHistoricalLocationsCollectionByThing(string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<HistoricalLocation>>(typeof(HistoricalLocation), typeof(Thing), id,
+                odata);
         }
 
-        public async Task<Response<Datastream>> GetDatastream(string id, OdataQuery odata = null)
-        {
-            return await Get<Datastream>(EntityType.Datastream, id, odata);
+        public async Task<Response<Datastream>> GetDatastream(string id, OdataQuery odata = null) {
+            return await Get<Datastream>(typeof(Datastream), id, odata);
         }
 
-        public async Task<Response<Datastream>> GetDatastreamByObservation(string id, OdataQuery odata = null)
-        {
-            return await Get<Datastream>(EntityType.Datastream, EntityType.Observation, id, odata);
+        public async Task<Response<Datastream>> GetDatastreamByObservation(string id, OdataQuery odata = null) {
+            return await Get<Datastream>(typeof(Datastream), typeof(Observation), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Datastream>>> GetDatastreamCollection(OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Datastream>>(EntityType.Datastream, odata);
+        public async Task<Response<SensorThingsCollection<Datastream>>>
+            GetDatastreamCollection(OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Datastream>>(typeof(Datastream), odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Datastream>>> GetDatastreamCollectionByObservedProperty(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Datastream>>(EntityType.Datastream, EntityType.ObservedProperty, id, odata);
+        public async Task<Response<SensorThingsCollection<Datastream>>>
+            GetDatastreamCollectionByObservedProperty(string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Datastream>>(typeof(Datastream), typeof(ObservedProperty), id,
+                odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Datastream>>> GetDatastreamCollectionBySensor(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Datastream>>(EntityType.Datastream, EntityType.ObservedProperty, id, odata);
+        public async Task<Response<SensorThingsCollection<Datastream>>>
+            GetDatastreamCollectionBySensor(string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Datastream>>(typeof(Datastream), typeof(ObservedProperty), id,
+                odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Datastream>>> GetDatastreamCollectionByThing(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Datastream>>(EntityType.Datastream, EntityType.Thing, id, odata);
+        public async Task<Response<SensorThingsCollection<Datastream>>>
+            GetDatastreamCollectionByThing(string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Datastream>>(typeof(Datastream), typeof(Thing), id, odata);
         }
 
-        public async Task<Response<Sensor>> GetSensor(string id, OdataQuery odata = null)
-        {
-            return await Get<Sensor>(EntityType.Sensor, id, odata);
+        public async Task<Response<Sensor>> GetSensor(string id, OdataQuery odata = null) {
+            return await Get<Sensor>(typeof(Sensor), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Sensor>>> GetSensorCollection(OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Sensor>>(EntityType.Sensor, odata);
+        public async Task<Response<SensorThingsCollection<Sensor>>> GetSensorCollection(OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Sensor>>(typeof(Sensor), odata);
         }
 
-        public async Task<Response<Sensor>> GetSensorByDatastream(string id, OdataQuery odata = null)
-        {
-            return await Get<Sensor>(EntityType.Sensor,EntityType.Datastream, id, odata);
+        public async Task<Response<Sensor>> GetSensorByDatastream(string id, OdataQuery odata = null) {
+            return await Get<Sensor>(typeof(Sensor), typeof(Datastream), id, odata);
         }
 
-        public async Task<Response<ObservedProperty>> GetObservedProperty(string id, OdataQuery odata = null)
-        {
-            return await Get<ObservedProperty>(EntityType.ObservedProperty, id, odata);
+        public async Task<Response<ObservedProperty>> GetObservedProperty(string id, OdataQuery odata = null) {
+            return await Get<ObservedProperty>(typeof(ObservedProperty), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<ObservedProperty>>> GetObservedPropertyCollection(OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<ObservedProperty>>(EntityType.ObservedProperty, odata);
+        public async Task<Response<SensorThingsCollection<ObservedProperty>>>
+            GetObservedPropertyCollection(OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<ObservedProperty>>(typeof(ObservedProperty), odata);
         }
 
-        public async Task<Response<ObservedProperty>> GetObservedPropertyByDatastream(string id, OdataQuery odata = null)
-        {
-            return await Get<ObservedProperty>(EntityType.ObservedProperty, EntityType.Datastream, id, odata);
+        public async Task<Response<ObservedProperty>> GetObservedPropertyByDatastream(
+            string id, OdataQuery odata = null) {
+            return await Get<ObservedProperty>(typeof(ObservedProperty), typeof(Datastream), id, odata);
         }
 
-        public async Task<Response<Observation>> GetObservation(string id, OdataQuery odata = null)
-        {
-            return await Get<Observation>(EntityType.Observation, id, odata);
+        public async Task<Response<Observation>> GetObservation(string id, OdataQuery odata = null) {
+            return await Get<Observation>(typeof(Observation), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Observation>>> GetObservationCollection(OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Observation>>(EntityType.Observation, odata);
+        public async Task<Response<SensorThingsCollection<Observation>>>
+            GetObservationCollection(OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Observation>>(typeof(Observation), odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Observation>>> GetObservationCollectionByFeatureOfInterest(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Observation>>(EntityType.Observation, EntityType.FeatureOfInterest, id, odata);
+        public async Task<Response<SensorThingsCollection<Observation>>>
+            GetObservationCollectionByFeatureOfInterest(string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Observation>>(typeof(Observation), typeof(FeatureOfInterest), id,
+                odata);
         }
 
-        public async Task<Response<SensorThingsCollection<Observation>>> GetObservationCollectionByDatastream(string id, OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<Observation>>(EntityType.Observation, EntityType.Datastream, id, odata);
+        public async Task<Response<SensorThingsCollection<Observation>>>
+            GetObservationCollectionByDatastream(string id, OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<Observation>>(typeof(Observation), typeof(Datastream), id, odata);
         }
 
-        public async Task<Response<FeatureOfInterest>> GetFeatureOfInterest(string id, OdataQuery odata = null)
-        {
-            return await Get<FeatureOfInterest>(EntityType.FeatureOfInterest, id, odata);
+        public async Task<Response<FeatureOfInterest>> GetFeatureOfInterest(string id, OdataQuery odata = null) {
+            return await Get<FeatureOfInterest>(typeof(FeatureOfInterest), id, odata);
         }
 
-        public async Task<Response<FeatureOfInterest>> GetFeatureOfInterestByObservation(string id, OdataQuery odata = null)
-        {
-            return await Get<FeatureOfInterest>(EntityType.FeatureOfInterest, EntityType.Observation, id, odata);
+        public async Task<Response<FeatureOfInterest>> GetFeatureOfInterestByObservation(
+            string id, OdataQuery odata = null) {
+            return await Get<FeatureOfInterest>(typeof(FeatureOfInterest), typeof(Observation), id, odata);
         }
 
-        public async Task<Response<SensorThingsCollection<FeatureOfInterest>>> GetFeatureOfInterestCollection(OdataQuery odata = null)
-        {
-            return await Get<SensorThingsCollection<FeatureOfInterest>>(EntityType.FeatureOfInterest, odata);
+        public async Task<Response<SensorThingsCollection<FeatureOfInterest>>>
+            GetFeatureOfInterestCollection(OdataQuery odata = null) {
+            return await Get<SensorThingsCollection<FeatureOfInterest>>(typeof(FeatureOfInterest), odata);
         }
 
-        public async Task<Response<Observation>> CreateObservation(Observation observation)
-        {
+        public async Task<Response<Observation>> CreateObservation(Observation observation) {
             var url = _homedoc.GetUrlByEntityName("Observations");
             return await Http.PostJson<Observation>(url, observation);
         }
 
-        private async Task<Response<T>> Get<T>(EntityType get, OdataQuery odata = null)
-        {
-            return await Get<T>(get, EntityType.None, null, odata);
+        private async Task<Response<T>> Get<T>(Type get, OdataQuery odata = null) {
+            return await Get<T>(get, null, null, odata);
         }
 
-        private async Task<Response<T>> Get<T>(EntityType get, string id, OdataQuery odata = null)
-        {
-            return await Get<T>(get, EntityType.None, id, odata);
+        private async Task<Response<T>> Get<T>(Type get, string id = null, OdataQuery odata = null) {
+            return await Get<T>(get, null, id, odata);
         }
 
-        private async Task<Response<T>> Get<T>(EntityType get, EntityType by, string id, OdataQuery odata = null)
-        {
-            if (by != EntityType.None && string.IsNullOrEmpty(id))
-            {
+        private async Task<Response<T>> Get<T>(Type get, Type by = null, string id = null, OdataQuery odata = null) {
+            if (by != null && string.IsNullOrEmpty(id)) {
                 throw new Exception("ID is required");
             }
 
             string url;
             var idString = string.IsNullOrEmpty(id) ? "" : $"({id})";
 
-            if (by == EntityType.None)
-            {
+            if (by == null) {
                 url = $"{_homedoc.GetUrlByEntityName(get.GetString(true))}{idString}";
+            } else {
+                url = $"{_homedoc.GetUrlByEntityName(by.GetString(true))}{idString}/{get.GetString(by)}";
             }
-            else
-            {
-                if (EntityMapping.GetByMapping.ContainsKey(get) && EntityMapping.GetByMapping[get].ContainsKey(by))
-                {
-                    url = $"{_homedoc.GetUrlByEntityName(by.GetString(true))}{idString}/{EntityMapping.GetByMapping[get][by]}";
-                }
-                else
-                {
-                    throw new Exception("Path does not exist");
-                }
-            }
-
             url = odata != null ? odata.AppendOdataQueryToUrl(url) : url;
+
             return await Http.GetJson<T>(url);
         }
     }
