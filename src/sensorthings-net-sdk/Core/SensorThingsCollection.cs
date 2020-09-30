89168586adbefa3cf40a2492f5be163caa1e49ac
Danny Lade <danny.lade@iteratec.com>
2020-09-30 12:30:48 +0200

    Fixed: Warning CS0693: Type parameter 'T' has the same name as the type parameter from outer type.

--- src/sensorthings-net-sdk/Core/SensorThingsCollection.cs
+++ src/sensorthings-net-sdk/Core/SensorThingsCollection.cs
@@ -47,13 +47,12 @@ namespace SensorThings.Core
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        protected bool SetProperty<TS>(ref TS storage, TS value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(storage, value))
             {
                 return false;
             }
-
             storage = value;
             OnPropertyChanged(propertyName);
             return true;
