cbd8b2e7167f86786a9402b3c29f790450f1cf96
Danny Lade <danny.lade@iteratec.com>
2020-11-02 11:08:16 +0100

    Return Location in every response. For POST method (create) the location is replaced by real object location.

--- src/sensorthings-net-sdk/Core/Response.cs
+++ src/sensorthings-net-sdk/Core/Response.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace sensorthings.Core
 {
@@ -10,6 +11,11 @@ namespace sensorthings.Core
         /// Expected result from the service
         /// </summary>
         public T Result { get; set; }
+
+        /// <summary>
+        /// Location of the object.
+        /// </summary>
+        public Uri Location { get; set; }
 
         /// <summary>
         /// True if request was sucesfully and returned with expected result
@@ -32,34 +38,27 @@ namespace sensorthings.Core
         public HttpStatusCode HttpStatusCode { get; set; }
 
         /// <summary>
-        /// Create an empty response object
-        /// </summary>
-        /// <returns></returns>
-        public static Response<T> CreateEmpty()
-        {
-            return new Response<T>();
-        }
-
-        /// <summary>
         /// Create a successful response
         /// </summary>
+        /// <param name="location"></param>
         /// <param name="result"></param>
         /// <param name="httpStatusCode"></param>
         /// <returns></returns>
-        public static Response<T> CreateSuccessful(T result, HttpStatusCode httpStatusCode)
+        public static Response<T> CreateSuccessful(Uri location, T result, HttpStatusCode httpStatusCode)
         {
-            return new Response<T> { Success = true, Result = result, HttpStatusCode = httpStatusCode };
+            return new Response<T> { Success = true, Location = location, Result = result, HttpStatusCode = httpStatusCode };
         }
 
         /// <summary>
         /// Create an unsuccessful response
         /// </summary>
+        /// <param name="location"></param>
         /// <param name="serviceError"></param>
         /// <param name="httpStatusCode"></param>
         /// <returns></returns>
-        public static Response<T> CreateUnsuccessful(string serviceError, HttpStatusCode httpStatusCode)
+        public static Response<T> CreateUnsuccessful(Uri location, string serviceError, HttpStatusCode httpStatusCode)
         {
-            return new Response<T> { Success = false, Error = "See ServiceError", ServiceError = serviceError, HttpStatusCode = httpStatusCode };
+            return new Response<T> { Success = false, Location = location, Error = "See ServiceError", ServiceError = serviceError, HttpStatusCode = httpStatusCode };
         }
     }
 }
